9b2cea11039e3d332688a93acba02158f28aa151
DaphneZhu <61849400+DaphneZhu@users.noreply.github.com>
2020-07-17 00:05:15 -0500

    test

--- queries.sql
+++ queries.sql
@@ -1,7 +1,21 @@
+Queries.sql
 ## Part 1: Test it with SQL
+SELECT *
+FROM tech_jobs.job;
 
 ## Part 2: Test it with SQL
+SELECT name
+FROM tech_jobs.employer
+WHERE location = St. Louis City;
 
 ## Part 3: Test it with SQL
 
+DROP TABLE tech_jobs.job;
+
 ## Part 4: Test it with SQL
+
+SELECT name, description
+FROM tech_jobs.skill
+INNER JOIN tech_jobs.job
+ON tech_jobs.skill.name = tech_jobs.job.name
+WHERE name NOT NULL;
